a96088ebb5bcedbf81333d911f6b3da6f59eb080
Tim Davis <timdavish@gmail.com>
2018-01-26 11:44:07 -0800

    Problem: uniq #2

---
+++ scripts/uniq-#2-sed.sh
@@ -0,0 +1,4 @@
+#!/usr/local/bin/bash
+# Given a text file, count the number of times each line repeats itself. Only consider consecutive repetitions. Display the space separated count and line, respectively. There shouldn't be any leading or trailing spaces.
+
+uniq -c | sed -e 's/^[ ]*//'
